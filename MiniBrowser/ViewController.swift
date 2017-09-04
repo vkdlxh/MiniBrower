07c9aa1f2ae9de0e797f45ab513e48393f262bb8
김태현 <kim@kimui-MacBook-Pro.local>
2017-09-04 17:21:11 +0900

    added webview and segmented control

--- MiniBrowser/ViewController.swift
+++ MiniBrowser/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: UIWebView!
+    
+    @IBAction func bookMarkAction(sender: AnyObject) {
+        let bookMarkUrl = bookMarkSegmentedControl.titleForSegmentAtIndex(bookMarkSegmentedControl.selectedSegmentIndex)
+        let urlString = "http://www.\(bookMarkUrl).com"
+        mainWebView.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,7 +28,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // commit test.
     }
 
 
